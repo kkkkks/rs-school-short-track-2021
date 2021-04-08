34fef29c284a3b62fe9da9b5709e140b09f5a3d4
kkkkks <64839061+kkkkks@users.noreply.github.com>
2021-04-08 13:00:01 +0300

    feat: add solution to task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,23 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let low = 0;
+  let high = array.length - 1;
+
+  while (low <= high) { // Бинарный поиск
+    const mid = Math.floor((low + high) / 2);
+    const guess = array[mid];
+
+    if (guess === value) {
+      return mid;
+    } if (guess > value) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = findIndex;
